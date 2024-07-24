20072f81c4000980d4731a0eca07d7d8ab42c1e0
Gustavo Magalhaes <117767677+gustavomgcz@users.noreply.github.com>
2024-07-24 13:45:39 -0300

    Update index.js

--- frontend/src/components/Banner/index.js
+++ frontend/src/components/Banner/index.js
@@ -5,7 +5,7 @@ function Banner() {
         <div className='banner container'>
             <div className='banner-grid'>
                 <h1 className='banner-title'>
-                    Segurança da informação, meu pau na sua mão
+                    Segurança da informação
                 </h1>
                 <p className='banner-text'>
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean vel dolor venenatis, vestibulum lorem nec, ultricies sapien. Mauris fringilla risus ut odio sollicitudin, nec euismod ex varius. Integer non lacus ac mi tempor dapibus.
